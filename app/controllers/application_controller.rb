0adf1e46d425442ea4cf349ca79d2f63d7fdfde1
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-19 04:11:34 +0000

    add new tea form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,10 +33,29 @@ class ApplicationController < Sinatra::Base
     erb :'teas/index'
   end
 
+  get '/teas/new' do
+    erb :'teas/new'
+  end
 
+  post '/teas' do
+    redirect '/teas/#{@tea.id}'
+  end
 
+  get '/teas/:id/edit' do
+    erb :'teas/edit'
+  end
 
+  post '/teas/:id' do
+    redirect '/teas/#{@tea.id}'
+  end
 
+  get '/teas/:id' do
+    erb :'teas/show'
+  end
+
+  get '/logout' do
+    session.clear
+  end
 
   helpers do
     def current_user
