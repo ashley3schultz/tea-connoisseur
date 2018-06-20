ffcc1b1a221295b49f4fc060e6ce37dfbc414da0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-20 18:35:55 +0000

    add login authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,39 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    if logged_in?
-      redirect '/teas'
-    else
-      erb :'users/signup'
-    end
-  end
-
-  post '/signup' do
-    if User.find_by(username: params[:username]) || User.find_by(email: params[:email])
-      redirect '/login'
-    else
-      @user =  User.create(params)
-      session[:user_id] = @user.id
-      redirect '/teas'
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/teas'
-    else
-      erb :'users/login'
-    end
-  end
-
-  post '/login' do
-      @user = User.find_by(username: params[:username])
-      @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/teas'
-  end
-
   get '/teas' do
     erb :'teas/index'
   end
@@ -68,11 +35,6 @@ class ApplicationController < Sinatra::Base
 
   get '/teas/:id' do
     erb :'teas/show'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect '/'
   end
 
   helpers do
