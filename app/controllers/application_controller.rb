48ee37bf906b0575e19eaa6d02bf42da36d94901
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-21 16:18:11 +0000

    change current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
 
     def logged_in?
